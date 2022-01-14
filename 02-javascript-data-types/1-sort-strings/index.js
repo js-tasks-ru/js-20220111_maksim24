c4d735f50be0feb08e155f70c6c4aab7c37272c2
MaksymMV <maksym.maslakov@mppteam.com>
2022-01-15 00:46:32 +0200

    solve batch of tasks

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,11 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const result = [...arr].sort((a, b) => a.localeCompare(b, ['ru', 'en'], {caseFirst: 'upper'}));
 
+  if (param === "asc") {
+    return result;
+  } else if (param === "desc") {
+    return result.reverse();
+  }
 }
